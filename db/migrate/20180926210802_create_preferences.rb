5e1ac4f8eeb0646a6e319791f70b3b4465b9326b
Alex Zdatny <alexzdatny@gmail.com>
2018-09-26 21:08:59 +0000

    automatically backed up by learn

--- db/migrate/20180926210802_create_preferences.rb
+++ db/migrate/20180926210802_create_preferences.rb
@@ -3,6 +3,8 @@ class CreatePreferences < ActiveRecord::Migration
     create_table :preferences do |t|
       t.string :song_sort_order
       t.string :artist_sort_order
+      t.string :allow_create_songs
+      t.boolean :allow_create_artists
     end
   end
 end
